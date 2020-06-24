7a5e1c870d34f91a7ed09a74206d80564eb49cd0
Fabrizzio Rivera <frivera@nevicad.com>
2020-06-24 00:12:54 -0600

    client url

--- src/providers/security/security.ts
+++ src/providers/security/security.ts
@@ -15,7 +15,7 @@ export class SecurityProvider {
         );
         userCredentials.communicationAddress = userInfo.CommunicationAddress;
         */ 
-        userCredentials.communicationAddress = "http://localhost:6661"  
+        userCredentials.communicationAddress = "http://10.240.29.104:8099"  
         return this.api.login(userCredentials);
     }
 }
